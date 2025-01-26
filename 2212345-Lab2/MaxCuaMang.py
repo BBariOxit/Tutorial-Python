eebfb1d2e9cc5c109f95facc45f003197c719ed9
Phan Thai Bao <phanbao2648@gmail.com>
2025-01-26 15:12:40 +0700

    update9

--- 2212345-Lab2/MaxCuaMang.py
+++ 2212345-Lab2/MaxCuaMang.py
@@ -1,16 +1,16 @@
 #CACH1:normalnormal
-# txt = input("nhap day so nguyen cach nhau boi dau phay:")
-# Ds = list(txt.split(','))
-# Dsnew = []
-# for i in Ds:
-#     Dsnew.append(int(i))
-# def MaxCuaMang(Dsnew):
-#     a = 0
-#     for i in Dsnew:
-#         if(i > a):
-#             a = i
-#     return a
-# print(f"phan tu lon nhat cua mang {Ds} la:", MaxCuaMang(Dsnew))
+txt = input("nhap day so nguyen cach nhau boi dau phay:")
+Ds = list(txt.split(','))
+Dsnew = []
+for i in Ds:
+    Dsnew.append(int(i))
+def MaxCuaMang(Dsnew):
+    a = 0
+    for i in Dsnew:
+        if(i > a):
+            a = i
+    return a
+print(f"phan tu lon nhat cua mang {Ds} la:", MaxCuaMang(Dsnew))
 
 #CACH2:arr[i]
 def laregest(arr,n):
